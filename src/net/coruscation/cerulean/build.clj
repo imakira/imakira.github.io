e61f31a4fe2b08019d780e805df06e90ccbf7b99
Akira <coruscationmail@gmail.com>
2025-12-14 01:55:23 +0800

    basic support for hot reloading orgx files

--- src/net/coruscation/cerulean/build.clj
+++ src/net/coruscation/cerulean/build.clj
@@ -1,9 +1,27 @@
 (ns net.coruscation.cerulean.build
   (:require
    [babashka.process :as process]
+   [clojure.core.async :as a]
+   [net.coruscation.cerulean.config :as config]
    [net.coruscation.cerulean.orgx.orgx :as orgx]
    [net.coruscation.cerulean.server.assets :as assets :refer [fetch-all-blogs]]
+   [net.coruscation.cerulean.server.watch-service :as watch-service]
    [shadow.build.targets.esm :as esm]))
+
+(defn generate-all-orgx! []
+  (doseq [blog (fetch-all-blogs)]
+    (when (:blog/orgx blog)
+      (orgx/generate-cljc-from-blog blog))))
+
+(def maybe-init-orgx-watch!
+  (memoize
+   (fn []
+     (future
+       (let [[resp cancel] (watch-service/watch config/*blog-dir*)]
+         (loop [event (a/<!! resp)]
+           (when (not (nil? event))
+             (generate-all-orgx!)
+             (recur (a/<!! resp)))))))))
 
 (defn build-css-hook {:shadow.build/stage :compile-prepare}
   [build-state & _]
@@ -12,6 +30,8 @@
 
 (defn orgx-hook {:shadow.build/stage :configure}
   [build-state & _]
+  (maybe-init-orgx-watch!)
+  (generate-all-orgx!)
   (let [modules (merge (get-in build-state [:shadow.build/config :modules])
                        #_{:demonstration
                           {:entries
@@ -22,7 +42,6 @@
                        (->> (fetch-all-blogs)
                             (map (fn [blog]
                                    (when (:blog/orgx blog)
-                                     (orgx/generate-cljc-from-blog blog)
                                      [(keyword (str "orgx." (:blog/id blog)))
                                       {:entries
                                        [(symbol (str "orgx." (:blog/id blog)))]
