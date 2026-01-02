c3d5262c3c4b466650cd45ed4b2aef1f7b9d1747
Akira <coruscationmail@gmail.com>
2026-01-03 01:47:09 +0800

    a new blog

---
+++ docs/js/orgx.build-something-similar-to-mdx-but-with-clojure-and-org-mode-for-my-personal-blog.js
@@ -0,0 +1,13 @@
+
+import "./shared.js";
+import "./default.js";
+import "./cljs-runtime/shadow.module.orgx.build-something-similar-to-mdx-but-with-clojure-and-org-mode-for-my-personal-blog.prepend.js";
+SHADOW_ENV.setLoaded("shadow.module.orgx.build-something-similar-to-mdx-but-with-clojure-and-org-mode-for-my-personal-blog.prepend.js");
+import "./cljs-runtime/orgx.build_something_similar_to_mdx_but_with_clojure_and_org_mode_for_my_personal_blog.js";
+SHADOW_ENV.setLoaded("orgx.build_something_similar_to_mdx_but_with_clojure_and_org_mode_for_my_personal_blog.js");
+import "./cljs-runtime/shadow.module.orgx.build-something-similar-to-mdx-but-with-clojure-and-org-mode-for-my-personal-blog.append.js";
+SHADOW_ENV.setLoaded("shadow.module.orgx.build-something-similar-to-mdx-but-with-clojure-and-org-mode-for-my-personal-blog.append.js");
+export default orgx.build_something_similar_to_mdx_but_with_clojure_and_org_mode_for_my_personal_blog.component;
+export let component = orgx.build_something_similar_to_mdx_but_with_clojure_and_org_mode_for_my_personal_blog.component;
+
+shadow.cljs.devtools.client.env.module_loaded("orgx.build-something-similar-to-mdx-but-with-clojure-and-org-mode-for-my-personal-blog");
