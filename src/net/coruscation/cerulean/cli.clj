1db435c4bc70bd4b5ba5b516310d44e7ed01b83e
Akira <coruscationmail@gmail.com>
2026-01-12 17:27:35 +0800

    fix adding classpath when not in repl

--- src/net/coruscation/cerulean/cli.clj
+++ src/net/coruscation/cerulean/cli.clj
@@ -1,12 +1,14 @@
 (ns net.coruscation.cerulean.cli
   (:require
    [clojure.java.io :as io]
+   [clojure.tools.cli :refer [parse-opts]]
    [net.coruscation.cerulean.config :refer [*blog-dir* *workspace*
                                             join-workspace-path]]
+   [net.coruscation.cerulean.server.server :as server]
    [net.coruscation.cerulean.server.static-generator :as static-generator]
    [net.coruscation.cerulean.server.utils :refer [path-join]]
-   [net.coruscation.cerulean.server.server :as server]
-   [clojure.tools.cli :refer [parse-opts]])
+   [shadow.cljs.devtools.api :as shadow]
+   [shadow.cljs.devtools.server :as shadow.server])
   (:gen-class))
 
 (defonce config-file-name "config.edn")
@@ -30,11 +32,15 @@
   (static-generator/build-full))
 
 (defn watch-workspace [& _]
-  (server/start-server!))
+  (server/start-server!)
+  (shadow.server/start!)
+  (shadow/watch :app))
 
 (def cli-options-root [])
 
 (defn -main [& args]
+  (let [cl (.getContextClassLoader (Thread/currentThread))]
+    (.setContextClassLoader (Thread/currentThread) (clojure.lang.DynamicClassLoader. cl)))
   (let [{:keys [arguments]} (parse-opts args cli-options-root)
         [subcmd & subcmd-args] arguments]
     (apply (case (keyword subcmd)
@@ -42,3 +48,10 @@
              :build build-workspace
              :watch watch-workspace)
            subcmd-args)))
+
+
+(defn -build [& _]
+  (-main "build"))
+
+(defn -watch [& _]
+  (-main "watch"))
