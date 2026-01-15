f2ddd40682710f156814733eab7930d9287bb505
Akira <coruscationmail@gmail.com>
2026-01-15 16:57:12 +0800

    fix orgx tests

--- test/net/coruscation/cerulean/orgx/orgx_test.clj
+++ test/net/coruscation/cerulean/orgx/orgx_test.clj
@@ -1,11 +1,17 @@
 (ns net.coruscation.cerulean.orgx.orgx-test
   (:require
-   [clojure.test :refer [deftest is]]
+   [clojure.test :refer [deftest is testing]]
    [net.coruscation.cerulean.orgx.orgx :as sut]))
 
 (deftest from-html-test
-  (is (= (str (sut/from-html "<pre class=\"uix\">  ($ :button {:on-click (fn [&amp; _] (js/alert &quot;clicked!&quot;))} &quot;Click Me&quot;) </pre>"))
-         "[($ :button {:on-click (fn [& _] (js/alert \"clicked!\"))} \"Click Me\")]"))
+  (testing
+      (let [[[ele] toplevels] (sut/from-html "<pre class=\"orgx\">  ($ :button {:on-click (fn [&amp; _] (js/alert &quot;clicked!&quot;))} &quot;Click Me&quot;) </pre>")]
+        (is (= (str ele)
+               "($ :button {:on-click (fn [& _] (js/alert \"clicked!\"))} \"Click Me\")"))
+        (is (not (seq toplevels))))))
 
-  (is (= (str (sut/from-html "<pre class=\"uix\">  ($ :button {:on-click (fn [&amp; _] #?(:cljs (js/alert &quot;clicked!&quot;)))} &quot;Click Me&quot;)</pre>"))
-         "[($ :button {:on-click (fn [& _] #?(:cljs (js/alert \"clicked!\")))} \"Click Me\")]")))
+(deftest toplevesl-test
+  (testing
+      (let [[_ toplevels] (sut/from-html "<pre class=\"orgx\" data-toplevel> (defui demo []) </pre> ")]
+        (is (= (str toplevels)
+               (str '((defui demo []))))))))
