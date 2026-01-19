b52086aa6ad8656b5ecb0344b9760d0a7ae56eed
Akira <coruscationmail@gmail.com>
2026-01-19 21:48:46 +0800

    do not rely on Make in css building process

--- src/net/coruscation/cerulean/server/tools.clj
+++ src/net/coruscation/cerulean/server/tools.clj
@@ -4,11 +4,11 @@
    [clojure.tools.logging :as logging]))
 
 (defn watch-css! []
-  (process/process "make styles-dev"
-                   {:out (logging/log-stream :info *ns*)
-                    :err (logging/log-stream :info *ns*)}))
+  (process/process "npx @tailwindcss/cli -m -i ./resources/main.css -o ./public/main.css"
+                   {:out (logging/log-stream :info 'net.coruscation.cerulean.server.tools)
+                    :err (logging/log-stream :info 'net.coruscation.cerulean.server.tools)}))
 
 (defn release-css! []
-  (process/process "make styles-release"
-                   {:out (logging/log-stream :info *ns*)
-                    :err (logging/log-stream :info *ns*)}))
+  (process/process "npx @tailwindcss/cli -m -i ./resources/main.css -o ./public/main.css"
+                   {:out (logging/log-stream :info 'net.coruscation.cerulean.server.tools)
+                    :err (logging/log-stream :info 'net.coruscation.cerulean.server.tools)}))
