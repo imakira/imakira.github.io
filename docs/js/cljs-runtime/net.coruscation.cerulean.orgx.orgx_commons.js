68c0208e32039f0493efe3e8d212048a651f6e9a
Akira <coruscationmail@gmail.com>
2026-01-26 05:12:38 +0800

    bulid site

--- docs/js/cljs-runtime/net.coruscation.cerulean.orgx.orgx_commons.js
+++
@@ -1,7 +0,0 @@
-import "./cljs_env.js";
-import "./cljs.core.js";
-goog.provide('net.coruscation.cerulean.orgx.orgx_commons');
-net.coruscation.cerulean.orgx.orgx_commons.orgx_base_ns = "orgx";
-net.coruscation.cerulean.orgx.orgx_commons.orgx_default_component_name = "component";
-
-//# sourceMappingURL=net.coruscation.cerulean.orgx.orgx_commons.js.map
--- docs/js/cljs-runtime/net.coruscation.cerulean.variables.js
+++
@@ -1,6 +0,0 @@
-import "./cljs_env.js";
-import "./cljs.core.js";
-goog.provide('net.coruscation.cerulean.variables');
-net.coruscation.cerulean.variables._STAR_rss_STAR_ = false;
-
-//# sourceMappingURL=net.coruscation.cerulean.variables.js.map
